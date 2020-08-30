7c38d6af5bd8b5b26b267d9cfdf511e36742c3fa
stigsanek <stigsanek@yandex.ru>
2020-08-30 23:50:46 +0400

    Добавляет сборку frontend

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css';
+import './css/app.css';
+
+import './../node_modules/jquery/dist/jquery.min';
+import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
+import './../node_modules/bootstrap-select/dist/js/bootstrap-select.min';
+import './../node_modules/bootstrap-select/dist/js/i18n/defaults-ru_RU.min';
+import './js/app';
