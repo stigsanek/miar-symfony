edd9daf605eaf25a3a68732f2f722573d0b90191
stigsanek <stigsanek@yandex.ru>
2020-09-14 18:14:41 +0400

    Реализует метод получения данных по участкам с присоединением всех справочников

--- src/Repository/PlotRepository.php
+++ src/Repository/PlotRepository.php
@@ -19,6 +19,41 @@ class PlotRepository extends ServiceEntityRepository
         parent::__construct($registry, Plot::class);
     }
 
+    /**
+     * @return Plot[] Returns an array of Plot objects
+     */
+    public function findAllWithJoin()
+    {
+        return $this->getEntityManager()->createQueryBuilder()->select(
+            'p.id, p.identifier, p.announcementText, p.permittedUse, p.permittedUseDoc, p.address, p.locality, '
+                . 'p.price, p.area, p.unitPrice, p.cadastralNumber, p.urlLink, p.placementDate, p.relevanceDate, '
+                . 'p.screenshotLink, p.commentAttribute, p.commentProcessing, p.commentGeneral, p.processingDate'
+        )
+            ->addSelect(
+                'a.name attribute, b.name bell, ca.name category, cl.name classifier, coe.name eCommunic, '
+                    . 'cow.name wCommunic, cog.name gCommunic, coh.name hCommunic, cor.name rCommunic, '
+                    . 'd.name district, so.name soucre, st.name state, u.lastName user, lsu.lastName lastSaveUser'
+            )
+            ->from('App\Entity\Plot', 'p')
+            ->join('App\Entity\Attribute', 'a', 'WITH', 'p.attribute = a.id')
+            ->join('App\Entity\Bell', 'b', 'WITH', 'p.bell = b.id')
+            ->join('App\Entity\Category', 'ca', 'WITH', 'p.category = ca.id')
+            ->join('App\Entity\Classifier', 'cl', 'WITH', 'p.classifier = cl.id')
+            ->join('App\Entity\Communication', 'coe', 'WITH', 'p.electricityCommunication = coe.id')
+            ->join('App\Entity\Communication', 'cow', 'WITH', 'p.waterCommunication = cow.id')
+            ->join('App\Entity\Communication', 'cog', 'WITH', 'p.gasCommunication = cog.id')
+            ->join('App\Entity\Communication', 'coh', 'WITH', 'p.heatingCommunication = coh.id')
+            ->join('App\Entity\Communication', 'cor', 'WITH', 'p.roadCommunication = cor.id')
+            ->join('App\Entity\District', 'd', 'WITH', 'p.district = d.id')
+            ->join('App\Entity\Source', 'so', 'WITH', 'p.source = so.id')
+            ->join('App\Entity\State', 'st', 'WITH', 'p.state = st.id')
+            ->join('App\Entity\User', 'u', 'WITH', 'p.performerUser = u.id')
+            ->join('App\Entity\User', 'lsu', 'WITH', 'p.lastSavePerformerUser = lsu.id')
+            ->orderBy('p.id')
+            ->getQuery()
+            ->getResult();
+    }
+
     // /**
     //  * @return Plot[] Returns an array of Plot objects
     //  */
