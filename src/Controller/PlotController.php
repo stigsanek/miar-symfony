71de96073d68baa8dda4ab353be7ba984eb771a0
stigsanek <stigsanek@yandex.ru>
2020-09-15 17:51:00 +0400

    Добавляет шаблон для пагинации, настраивает шаблон для страницы участков

--- src/Controller/PlotController.php
+++ src/Controller/PlotController.php
@@ -10,20 +10,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PlotController extends AbstractController
 {
+    private const PAGE_LIMIT = 10;
+
     /**
      * @Route("/plots", name="plot_index")
      */
-    public function index(PlotRepository $repository, PaginatorInterface $paginator, Request $request)
+    public function getAll(PlotRepository $repository, PaginatorInterface $paginator, Request $request)
     {
         $pagination = $paginator->paginate(
             $repository->findAllWithJoin(),
             $request->query->getInt('page', 1),
-            10
+            self::PAGE_LIMIT
         );
 
         return $this->render('plot/index.html.twig', [
-            'controller_name' => 'PlotController',
-            //'pagination' => $pagination
+            'pagination' => $pagination
+        ]);
+    }
+
+    /**
+     * @Route("/plots/{id}", name="plot_item")
+     */
+    public function getItem($id)
+    {
+        return $this->render('plot/item.html.twig', [
+            'id' => $id
         ]);
     }
 }
